a1c5846e7b2d8aaa5e821b2ef194d79b017ac4b2
Ayoobz123 <mohamadayoob93@gmail.com>
2023-02-08 13:04:46 -0800

    initiated

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+<div class="row">
+    <div class="col-2 pt-5 pb-5" style="background:#216da0;">
+        <img src="images/qept_logo_s.png" class="mt-4" alt="">
+        <ul class="text-light decoration-none mt-4">
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">HOME</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">ABOUT US</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">DOMAIN NAMES</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">OUR SERVICES</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">OUR PRODUCTS</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">PROJECT & CLIENTS</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">NEWS</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">CAREERS</a>
+            <a href="#" class="list-group-item list-group-item-action active" aria-current="true">CONTACT US</a>
+        </ul>
+        <ul class="pt-4">
+            <a><img src="images/ico-fb.png" alt="facebook"></a>
+            <a><img src="images/ico-in.png" alt="linkedin"></a>
+            <a><img src="images/ico-twit.png" alt="twitter"></a>
+        </ul>
+        <img src="images/cra.png" class="ms-3 pt-2" style="width: 155px;">
+        <img src="images/fingertec.jpg" class="ms-3 pt-5" style="width: 155px;">
+        <div class="pt-5 ms-4">
+            <button type="button" class="btn btn-outline-light bg-light text-dark">SUBSCRIBE</button>
+        </div>
+        <div class="badge bg-dark text-wrap mt-5" style="width: 14rem;"><h5><a href="" class="text-decoration-none text-light">Online Store</a></h5></div>    
+    </div>
+<div class="col-10" style="background:#000000;">
+
+</div>
+</div>
+</body>
+</html>
